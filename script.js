6b45f253b7919a1d9f1a6a87bc9d3ccbfedbb2c3
ahmad essam <ahmad77essam@gmail.com>
2020-11-15 08:36:21 +0200

    make London open default page

--- script.js
+++ script.js
@@ -17,3 +17,6 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += "active";
 }
+
+// Make London open default page
+document.getElementById('defaultopen').click();
